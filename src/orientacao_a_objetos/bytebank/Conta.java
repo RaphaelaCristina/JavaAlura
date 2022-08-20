10ccde44210c6084f2b98c627cf167e2a23ec127
RaphaelaCristina <raphitasp@msn.com>
2022-08-20 20:26:41 -0300

    Orientação a Objetos

---
+++ src/orientacao_a_objetos/bytebank/Conta.java
@@ -0,0 +1,15 @@
+package orientacao_a_objetos.bytebank;
+
+public class Conta {
+
+    //atributos
+    
+    double saldo;
+    int agencia;
+    int numero;
+    String titular;
+
+    public static void main(String[] args) {
+
+    }
+}
